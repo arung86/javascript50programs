37b7f7d4205e78c4892dddd6b80b9f722b0800a2
Arun Kumar G <arunkumar.g86@gmail.com>
2020-04-07 07:39:17 +0530

    Update sumOfnumFromArrayNum.js

--- sumOfnumFromArrayNum.js
+++ sumOfnumFromArrayNum.js
@@ -1,3 +1,12 @@
+//   edit2:
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+
+///
 var array =[1,2,3,4];
 var sum=0;
 for(let i=0;i<array.length;i++){
